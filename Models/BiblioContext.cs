02677c846cbaf5d23288b489ce1576f3d1127586
mamarlink <mamarlink@gmail.com>
2016-03-27 13:15:05 -0400

    premiere version de mabiblio

---
+++ Models/BiblioContext.cs
@@ -0,0 +1,15 @@
+﻿using MaBiblio.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MaBiblio.DAL
+{
+    public class BiblioContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+       
+        public DbSet<Author> Authors { get; set; }
+
+
+    }
+}
